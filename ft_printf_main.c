23f8a3af9ce9b5763b8bb3f532e75eb924da834e
lky <HelloWorld@jeonminhoui-MacBook-Pro.local>
2021-02-19 13:26:58 +0900

    a

--- ft_printf_main.c
+++ ft_printf_main.c
@@ -8,20 +8,33 @@ int set_res(va_list ap, char **res)
     count = 0;
     if (ft_strchr(*res, 'd'))
     {
-        free(*res); //여기서 flag처리해주는 함수.. 실질적으로 각각 파싱기능으로 들어가는 함수 구현하는부분 시작
+        free(*res); //여기서 flag처리해주는 함수.. 구현해야함..실질적으로 각각 파싱기능으로 들어가는 함수 구현하는부분 시작
         *res = ft_itoa(va_arg(ap, int));
         count = ft_strlen(*res);
         write(1, *res, count);
+        free(*res);
     }
-    //else if (ft_strchr(*res, 'c'))
-    //else if (ft_strchr(*res, 'i'))
+    else if (ft_strchr(*res, 'c'))
+    {
+        free(*res);
+        **res = va_arg(ap, int);
+        count = sizeof(char);
+        write(1, *res, 1);
+    }
+    else if (ft_strchr(*res, 's'))
+    {
+        free(*res);
+        *res = va_arg(ap, char *);
+        count = ft_strlen(*res);
+        write(1, *res, count);
+    }
     return (count);
 }
 char    *set_dtarg(char *a, int *k)
 {
     char    *pt_type;
 
-    while (a[*k] != 'd' && a[*k] != 'c' && a[*k] != 'i')
+    while (a[*k] != 'd' && a[*k] != 'c' && a[*k] != 's')
         (*k)++;
     pt_type = (char *)malloc(((*k) + 2) * sizeof(char));
     if (pt_type)
@@ -72,6 +85,6 @@ int ft_printf(const char *format, ...)
 
 int main(void)
 {
-	ft_printf("asdfasdf %d as %d df",1818181818,19191919);
+	ft_printf("asdfasd%%f %d as %d df %c %s",1818181818,19191919,'%',"mom! i did it!!");
 	return (0);
 }
