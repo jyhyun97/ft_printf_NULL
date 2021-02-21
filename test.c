a666a5dc07ccce05022e7266afc83fdc92ff333e
lky <HelloWorld@jeonminhoui-MacBook-Pro.local>
2021-02-22 03:07:50 +0900

    a

--- test.c
+++ test.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
-int main ()
+int main(void)
 {
-	//int a = 3;
-	//float b = 3.0;
-	
-	printf("a\n",a);
-	//printf("%f\n",3);
-	//printf("%f\n",a);
-	//printf("%d\n",b);
-	return (0);
+        printf("%5%\n");
+        printf("% %\n");
+        printf("%%\n");
+        printf("%%\n");
+        return (0);
 }
