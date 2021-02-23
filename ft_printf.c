bc1eed699b7a7b288a3a00851d8900e00dde4c35
jso <jso@student.42seoul.kr>
2021-02-23 12:14:33 +0900

    add basic func

---
+++ ft_printf.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/23 11:59:39 by jso               #+#    #+#             */
+/*   Updated: 2021/02/23 12:11:45 by jso              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int	ft_printf(char *fmt, ...)
+{
+	va_list ap;
+	int cnt;
+
+	cnt = 0;
+	va_start(ap, fmt);
+	printf("%d\n", va_arg(ap, int));
+	va_end(ap);
+	++cnt;
+	return (cnt);
+}
+
+int main(void)
+{
+	char *str;
+
+	str = "Hello";
+	ft_printf(str, 'a');
+	return (0);
+}
