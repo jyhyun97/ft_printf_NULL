e08889e89bfcc892f369cb0a399e042b956c0a1b
repl.it user <replituser@example.com>
2021-02-25 10:14:02 +0000

    format_specifier_0.1

--- ft_printf_main.c
+++ ft_printf_main.c
@@ -1,6 +1,9 @@
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+#include "utils.c"
 // %[flag][width][.precision][type]
 
 typedef struct t_flag
@@ -8,66 +11,111 @@ typedef struct t_flag
   int   minus;
   int   zero;
   int   precision;
-  int   star;
   int   width;
 	char	type; 
+  //int   star;
 }				s_flag;
 
 s_flag g_flag;
 
 void init_flag()
 {
-  g_flag->minus = 0;//왼쪽정렬 껐켰
-  g_flag->zero = 0;//0 패딩 공백 패딩 껐켰
-  g_flag->precision = -1;//정밀도 옵션 껐켰 precision~
-  g_flag->star = 0;//너비 가변 인자로 받아오는 거
-  g_flag->width = 0;//너비 받아오는 건데 star옵션이랑 중복될 경우는? 어떤 게 우선
-  g_flag->type = 0;//csdiupxX%같은 거 담아 놓기
+  g_flag.minus = 0;//0은 오른쪽 정렬 1은 왼족 정렬
+  g_flag.zero = 0;//0 패딩 0일 떄 공백 1일 때 0으로 패딩
+  g_flag.precision = -1;//정밀도 있으면 기본 0, 뒤 숫자 width처럼 저장
+  g_flag.width = 0;//너비 *으로 받아오기 or atio 받아오기
+  g_flag.type = 0;//csdiupxX% 담아놓기
+  //g_flag->star;//굳이 필요 할까?
 }
 
-void check_flag(char *a)
+char *check_flag(char *a)
 {
-
   int i = 0;
-  while (a[i] != '\0' && a[i] == '-' && a[i] == '0')
+  while (a[i] != '\0' && (a[i] == '-' || a[i] == '0'))
   {
     if (a[i] == '0')
-      g_flag->zero = 1;
+      g_flag.zero = 1;
     if (a[i] == '-')
-      g_flag->minus = 1;
+      g_flag.minus = 1;
+  i++;
   }
+  return (a + i);
+  //i 반환? 일단은 void 해놨음
 }
 
-void check_width(char *a, va_list ap)
+char *check_width(char *a, va_list ap)
 {
+  int i = 0;  
+  if (a[i] != '\0' && (a[i] == '*' || (a[i] >= '0' && a[i] <= '9')))
+  {
+    if (a[i] == '*')
+    {
+      g_flag.width = va_arg(ap, int);
+      i++;
+    }
+    while (a[i] >= '0' && a[i] <= '9' && a[i] != '\0')
+    {
+      g_flag.width = (g_flag.width * 10) + (a[i] - '0');
+      i++;
+    }
+  }
+  return (a + i);
 }
-void check_width(char *a, va_list ap)
+
+char *check_precision(char *a, va_list ap)
 {
-  //*, 숫자인 동안 while
-
+  int i = 0;
+  if (a[i] == '.' && a[i] != '\0')
+  {
+    i++;
+    g_flag.precision = 0;
+    if (a[i] != '\0' && (a[i] == '*' || (a[i] >= '0' && a[i] <= '9')))
+    {
+      if (a[i] == '*')
+      {
+        g_flag.precision = va_arg(ap, int);
+        i++;
+      }
+      while (a[i] >= '0' && a[i] <= '9' && a[i] != '\0')
+      {
+        g_flag.precision = (g_flag.precision * 10) + (a[i] - '0');
+        i++;
+      }
+    }
+  }
+  return (a + i);
 }
-void check_precision()
+
+char *check_type(char *a)
 {
-
-  //.으로 시작해서 *, digit 체크
-
+  int i = 0;
+  if (a[i] == 'c' || a[i] == 's' || a[i] == 'p' || a[i] == 'd' || a[i] == 'i' || a[i] == 'u' || a[i] == 'x' || a[i] == 'X' || a[i] == '%')
+    g_flag.type = a[i];
+  i++;
+  return (a + i);
 }
-void check_type()
+int print_all(va_list ap)
 {
-  //csduipxX% 체크
+  printf("서식지정자 출력 시작\n");
+  printf("minus : %d\n", g_flag.minus);
+  printf("zero : %d\n", g_flag.zero);
+  printf("width : %d\n", g_flag.width);
+  printf("prec : %d\n", g_flag.precision);
+  printf("type : %c\n", g_flag.type);
+  printf("출력 끝\n");
+  return (1);
 }
-int print_all();//출력되는 글자 기억해놓기~~~
 
-int checker(va_list ap, char *a)//한 글자씩? 한 서식 지정자 단위씩?
+int checker(va_list ap, char *a)
 {
     int rst = 0;
 
-    init_flag();//플래그를 초기화시킨다
-    check_flag(&a));//플래그 나오는 동안 계속 안에서 돌려!!!! 예외사항도 고려를 해줘!
-    check_width(&a, ap);//얜 어차피 하나 밖에 없으니까 괜찮다 다만 플래그에서 * 있을 경우 고려~
-    check_precision(&a, ap);//점 나오면 0으로 지정하고 숫자 받기~!!
-    check_type(&a, ap);//얘가 안 나온다? 서식 지정자가 아니다 print all만들 때 꼭 참고
-    rst += print_all(ap);//서식지정자에 필요한 모든 정보가 모였으니 확인하고 처리 들어가기
+    init_flag();
+    a = check_flag(a);
+    a = check_width(a, ap);
+    a = check_precision(a, ap);
+    a = check_type(a);
+    rst += print_all(ap);//예외처리, 출력 등
     return (rst);
 }
 
@@ -79,10 +127,10 @@ int rst = 0; //반환되어야할 총 문자열 길이
 while (*format != '\0')
 {
     if (*format == '%')
-      rst += checker(ap, &format + 1);//서식 지정자인지 확인
+      rst += checker(ap, format + 1);//서식 지정자인지 확인
     else
     {
-      write(1, *format, 1);//
+      write(1, format, 1);//
       rst++;
     }
   format++;
@@ -90,19 +138,20 @@ while (*format != '\0')
 return (rst);
 }
 
-
 int ft_printf(const char *format, ...)
 {
   int rst;
+  char *a = ft_strdup(format);
   va_list ap;
   va_start(ap, format);
-  rst = print_rst(ap, format);
+  rst = print_rst(ap, a);
 	va_end(ap);
+  free (a);
   return (rst);
 }
 
 int main(void)
 {
-	ft_printf("aaa%caaa%daaa", 'a', 20);
+	ft_printf("aaa%-5.*caaa%0-*.*daaa", 2, 3, 4);
 	return (0);
 }
---
+++ utils.c
@@ -0,0 +1,29 @@
+size_t	ft_strlen(const char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char				*a;
+	unsigned int		i;
+
+	a = malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (a == 0)
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		a[i] = s1[i];
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+}
