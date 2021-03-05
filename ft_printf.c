a13f14c2dd4364a708273c6ccdbe2877f6c0842f
jso <jso@student.42seoul.kr>
2021-03-05 12:21:14 +0900

    add printf_rst func in ft_pirntf.c file

--- ft_printf.c
+++ ft_printf.c
@@ -6,34 +6,36 @@
 /*   By: jso <jso@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 11:59:39 by jso               #+#    #+#             */
-/*   Updated: 2021/03/02 16:00:30 by jso              ###   ########.fr       */
+/*   Updated: 2021/03/05 12:20:13 by jso              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <stdio.h>
+#include "ft_printf.h"
+
+void printf_rst(va_list ap, char *fmt)
+{
+	while (*fmt)
+	{
+		if (*fmt == '%')
+		{
+			++fmt;
+			checker(ap, &fmt);
+		}
+		else
+		{
+			write(1, fmt, 1);
+			++g_flag.count;
+		}
+		++fmt;
+	}
+}
 
 int	ft_printf(char *fmt, ...)
 {
 	va_list ap;
-	int n_byte;
-	char *dup_str;
 
 	va_start(ap, fmt);
-	dup_str = ft_strdup(fmt);
-	n_byte = print_rst(ap, dup_str);
+	print_rst(ap, fmt);
 	va_end(ap);
-	free (dup_str);
-	return (n_byte);
-}
-
-int main(void)
-{
-	char *str;
-	int ret;
-
-	str = "aaa%-5.*saaa%000*.*daaa\n";
-	ret = ft_printf(str, 2, "!@#$", 3, 4, 9999);
-	printf("return value\t: %d\n", ret);
-	return (0);
+	return (g_flags.count);
 }
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char *str;
+
+	str = "aaa%-5.*saaa%000*.*daaa\n";
+	ft_printf(str, 2, "!@#$", 3, 4, 9999);
+	printf("return value\t: %d\n", g_flag.count);
+	return (0);
+}
